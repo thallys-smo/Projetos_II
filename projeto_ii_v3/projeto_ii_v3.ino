18863c7b43b51f6fff6f4a23705ae234ad810f94
Thallys Oliveira <thallys.smo@usp.br>
2023-12-11 00:00:43 -0300

    Quase funcional - Bola pesada

--- projeto_ii_v3/projeto_ii_v3.ino
+++ projeto_ii_v3/projeto_ii_v3.ino
@@ -1,35 +1,42 @@
-float y;                   // variável de processo
-float setpoint = 19;       // setpoint  - valor entre 0 e 40 cm de tubo
-float k = 5;              // ganho de referência
-float h = 0.1;             // tempo de amostragem 
-float ti = 50;             // tempo de integração
-float kp = 6.5; //4; //k*(1+h/(2*ti)); // ganho proporcional
-float ki = 1.8;//0.7;  //k/ti;           //ganho integral 
-float kd = 4;//2; // k/20;
-float p = 0;
-float i = 0;
-float d = 0;
-float e = 0;
-float e_anterior = 0; 
+float d_bola;         // variável de processo
+float k = 5;          // Ganho de Referência
+float h = 0.1;        // Tempo de Amostragem 
+float ti = 50;        // Tempo de Integração
+
+/* Para a bola de Isopor:
+  - Kp = 4, Ki = 0.7 e Kd = 2
+  Para a bola de Ping-pong:
+  - Kp = 6.5, Ki = 1.8 e Kd = 4
+  Jeito antigo de definições das constantes:
+  - Kp = k*(1+h/(2*ti)), Ki = k/ti e Kd =  k/20    */
+
+float kp = 6.5;       // Ganho Proporcional
+float ki = 2.1;       // Ganho Integral 
+float kd = 4.7;       // Ganho Diferencial
+float p = 0;          // Variável Proporcional
+float i = 0;          // Variável Integrativa
+float d = 0;          // Variável Deferenciativa
+float e = 0;          // Erro
+float e_anterior = 0; // Erro Anterior
 
 // Definindo os parametros do PWM do Motor brushless
 
-const int freq = 50;
-const int canal_A = 0;
-const int resolucao = 12; 
-const int pin_Atuacao_A = 5; // pino input ESC
-int valor_pwm = 0; // input do AD
-int ciclo_A = 0; // valor do cilo do PWM
-
+const int freq = 50;         // Frequência do PWM de 50 Hz
+const int canal_A = 0;       // Usando o canal 0 da Esp32
+const int resolucao = 12;    // Resolução de 12 bits, 2 elevado a 12, 4096 posições 
+const int pin_Atuacao_A = 5; // Pino input ESC
+int valor_pwm = 0;           // Input do AD
+int ciclo_A = 0;             // Valor do cilo do PWM
 
 // Definindo os pinos dos sensores de distancia
 //const int trigger_mao = 33; //Trigger do sensor da mão
-//const int echo_mao = 35; //Echo do sensor da mão
-const int trigger_bola = 32; //Trigger do sensor da bola
-const int echo_bola = 34; //Echo do sensor da bola
+//const int echo_mao = 35;    //Echo do sensor da mão
+const int trigger_bola = 32;  //Trigger do sensor da bola
+const int echo_bola = 34;     //Echo do sensor da bola
 
-const double l_tubo = 40; //Comprimento do tubo em cm
-const int t_max = (int) (l_tubo * 3.5 / 0.034); //Tempo máximo de leitura do sensor
+// Tempo da onda do Ultrassonico
+const double l_tubo = 40;                       // Comprimento do tubo em cm
+const int t_max = (int) (l_tubo * 3.5 / 0.034); // Tempo Máximo de leitura do sensor
 
 void setup(){
 
@@ -43,10 +50,10 @@ void setup(){
   ledcWrite(canal_A, ciclo_A);
 
   // Calibrando o motor -> Parado durante 1s  
-  Serial.println("Calibrando");
   delay(2000);
-  float u = map(0, 0, 100, 205, 300);   
-  ledcWrite(canal_A, u);
+  Serial.println("Calibrando Motor");
+  delay(2000);
+  motor(0, 205, 300);
   delay(5000);
   Serial.println("Motor Calibrado -> Iniciando a operação");
   delay(5000);
@@ -60,6 +67,10 @@ void setup(){
 }
 
 void loop(){  
+
+  // setpoint  - valor entre 0 e 40 cm de tubo
+  float setpoint = 25;  
+
   // Distancia da bola
   digitalWrite(trigger_bola, LOW);
   delayMicroseconds(2);
@@ -68,11 +79,11 @@ void loop(){
   digitalWrite(trigger_bola, LOW);
   unsigned long t_bola = pulseIn(echo_bola, HIGH, t_max);// Lê o pino echo do sensor que retorna o tempo em microsegundos
   // Calculo da distancia da bola
-  y = t_bola * 0.034 / 2;
+  d_bola = t_bola * 0.034 / 2;
 
   // Erro
   e_anterior = e;
-  e = y - setpoint;
+  e = d_bola - setpoint;
   
   // Definição do Ganho Proporcional
   p = kp*e; 
@@ -84,39 +95,76 @@ void loop(){
   // Definição da Ação de controle
   float u = p + i + d;
 
+  /* Para a bola de Isopor:
+  - Min_Pwm = 245 e Max_Pwm = 270
+  Para a bola de Ping-pong:
+  - Min_Pwm = 280 e Max_Pwm = 320   */
+
   // Ação de Controle Aplicada
-  u = constrain(map(u, 0, 100, 280, 320), 280, 320);    //245, 270 isopor
-  ledcWrite(canal_A, u);
-
-  // Imprimindo as coisas
-  imprimindo(y, e, p, i, d, u);
-
+  u= motor(u, 282, 330); // 
   
+  // Imprimindo as coisas 
+  imprimindo(setpoint, l_tubo - d_bola, e, p, i, d, u);  
 }
 
-void imprimindo(float y, float e, float p, float i, float d, float u){
+float PID(float e, float d_bola, float setpoint, float kp, float ki, float kd, float p, float i, float d){
+  // Erro
+  e_anterior = e;
+  e = d_bola - setpoint;
+  
+  // Definição do Ganho Proporcional
+  p = kp*e; 
+  // Definição do Ganho Integral
+  i = i +(ki*h*e);
+  // Definição do ganho Derivativo
+  d = kd*(e-e_anterior);
+  
+  // Definição da Ação de controle
+  float u = p + i + d;
 
-  Serial.print("Distância da bola: ");
-  Serial.println(y);
+  return u;
+}
 
-  Serial.print("Erro: ");
-  Serial.println(e);
+float motor(float u, float min_pwm, float max_pwm){
 
-  Serial.print("Novo Kp: ");
-  Serial.println(p);
+  // Ação de Controle Aplicada
+  u = constrain(map(u, 0, 100, min_pwm, max_pwm), min_pwm, max_pwm);    
+  ledcWrite(canal_A, u);
+  return u;
+}
+// 
+void imprimindo(float setpoint, float d_bola, float e, float p, float i, float d, float u){
 
-  Serial.print("Novo Ki: ");
-  Serial.println(i);
+  //Serial.print("Setpoint: ");
+  Serial.print(setpoint);
+  Serial.print(" ");
 
-  Serial.print("Novo Kd: ");
-  Serial.println(d);
+  //Serial.print("Distância da bola: ");
+  Serial.print(d_bola);
+  Serial.print(" ");
+  
+  //Serial.print("Erro: ");
+  Serial.print(e);
+  Serial.print(" ");
 
-  Serial.print("Ação de controle (PID): ");
+  //Serial.print("Novo Kp: ");
+  Serial.print(p);
+  Serial.print(" ");
+
+  //Serial.print("Novo Ki: ");
+  Serial.print(i);
+  Serial.print(" ");
+
+  //Serial.print("Novo Kd: ");
+  Serial.print(d);
+  Serial.print(" ");
+
+  //Serial.print("Ação de controle (PID): ");
   Serial.println(u);
+  Serial.print(" ");
 
   delay(1000);
 
   Serial.print("\n");
-
+  
 }
-
